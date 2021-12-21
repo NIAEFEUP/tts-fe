1f88023d441163a346e657ff1b6742c0a26d13d4
kiko-g <kikojpgoncalves@gmail.com>
2021-12-21 02:56:48 +0000

    Update: Feup Exchange page and .prettier files

--- src/pages/FeupExchangePage.jsx
+++ src/pages/FeupExchangePage.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-import Selection from "../components/Selection";
+import React from 'react';
+import { makeStyles } from '@mui/styles';
+import Selection from '../components/Selection';
 
-const FeupExchangePage = () => (
-    <div>
-        Welcome to the FeupExchangePage!
-        <Selection />
+const useStyles = makeStyles((theme) => ({
+  container: {
+    margin: '2rem',
+  },
+}));
+
+const FeupExchangePage = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container}>
+      Welcome to the FeupExchangePage!
+      <Selection />
     </div>
-);
+  );
+};
 
 export default FeupExchangePage;
