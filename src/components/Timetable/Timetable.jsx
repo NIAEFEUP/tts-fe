14041a3a2977d0a4c3162310ad0dcef3f59e6154
Tiago Gomes <up201806658@fe.up.pt>
2022-02-20 08:50:38 +0000

    Remove: React Scheduler library

--- src/components/Timetable/Timetable.jsx
+++ src/components/Timetable/Timetable.jsx
@@ -1,37 +1,16 @@
 import React from "react";
 import { Grid } from "@mui/material";
-import { ViewState } from "@devexpress/dx-react-scheduler";
-import {
-    Scheduler,
-    WeekView,
-    Appointments,
-} from "@devexpress/dx-react-scheduler-material-ui";
 import { makeStyles } from "@mui/styles";
-import { TimetableConstants } from "../../utils/Timetable/TimetableConstants";
 
 const useStyles = makeStyles((theme) => ({
-    timetableSection: {
-        // padding: theme.spacing(5),
-    },
+    timetableSection: {},
 }));
 
 const Timetable = () => {
     const classes = useStyles();
     return (
         <Grid item xs={12} sm={9} className={classes.timetable}>
-            <Scheduler>
-                <ViewState />
-                <WeekView
-                    startDayHour={TimetableConstants.startDayHour}
-                    endDayHour={TimetableConstants.endDayHour}
-                    excludedDays={[
-                        TimetableConstants.weekdays.Sunday,
-                        TimetableConstants.weekdays.Saturday,
-                    ]}
-                    cellDuration={TimetableConstants.cellDurationMinutes}
-                />
-                <Appointments />
-            </Scheduler>
+            Timetable
         </Grid>
     );
 };
---
+++ src/components/Timetable/getTimetableData.js
@@ -0,0 +1,11 @@
+const timetableData = [
+    {
+        title: "ASSO",
+        startDate: Date.parse("2022-02-16T09:00:00"),
+        endDate: Date.parse("2022-02-16T11:30:00"),
+        id: 0,
+        location: "B105",
+    },
+];
+
+export default timetableData;
