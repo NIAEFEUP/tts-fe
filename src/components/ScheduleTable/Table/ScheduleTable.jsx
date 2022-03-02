61c2de67cb3a804e6dea7fda7fee5e8cb7fa0d1c
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-03-02 23:57:17 +0000

    Refactor: separate grid in a function

--- src/components/ScheduleTable/Table/ScheduleTable.jsx
+++ src/components/ScheduleTable/Table/ScheduleTable.jsx
@@ -14,29 +14,32 @@ const ScheduleTable = ({ selectedClasses }) => {
     );
 };
 
-
-
 const ScheduleBody = ({ selectedClasses }) => {
     const classes = timeTableStyles();
-    const hourList = getHourList();
-    const weekDays = headElements;
-    let borderTop = ""; 
-    
+
     return (
         <div className={classes.positionRelative}>
-            {hourList.map((hour, top) => {
-                return weekDays.map((day, left) => {
-                    if (left !== 0 && top !== 0) { 
-                        borderTop = "solid";
-                        let divClass = timeTableStyles({ top, left, borderTop });
-                        return <div className={divClass.border}></div>;
-                    } else return <></>
-                });
-            })} 
-            <Subjects selectedClasses={selectedClasses}/>
-            <Hours/>
+            <Grid />
+            <Subjects selectedClasses={selectedClasses} />
+            <Hours />
         </div>
     );
+};
+
+const Grid = () => {
+    const hourList = getHourList();
+    const weekDays = headElements;
+    let borderTop = "";
+
+    return hourList.map((hour, top) => {
+        return weekDays.map((day, left) => {
+            if (left !== 0 && top !== 0) {
+                borderTop = "solid";
+                let divClass = timeTableStyles({ top, left, borderTop });
+                return <div className={divClass.border}></div>;
+            } else return <></>;
+        });
+    });
 };
 
 export default ScheduleTable;
