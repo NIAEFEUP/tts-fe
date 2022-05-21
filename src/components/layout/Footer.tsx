869a87aabce03fbacd6cf8b390276d9de4f8a9e8
kiko-g <kikojpgoncalves@gmail.com>
2022-05-21 13:39:05 +0100

    footer styling

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,4 +1,4 @@
-import { LogoNIAEFEUPImage } from '../../images'
+import { LogoNIAEFEUPImage, LogoAEFEUPImage, LogoFEUPImage } from '../../images'
 import { GithubIcon, FacebookIcon, TwitterIcon, InstagramIcon, LinkedinIcon } from '../svgs'
 import { GlobeAltIcon, InboxInIcon } from '@heroicons/react/outline'
 
@@ -20,44 +20,60 @@ const socials: Social[] = [
 
 const Footer = () => {
   return (
-    <div className="bg-gray-700 px-2 py-3 dark:bg-dark md:px-5 md:py-3">
-      <div className="grid grid-cols-12 gap-4">
-        <div className="col-span-12 md:col-span-8">
-          <div className="flex items-center justify-center space-x-4 md:items-start md:justify-start">
-            <img src={LogoNIAEFEUPImage} alt="NIAEFEUP" className="h-auto w-12" />
-            <ul>
-              <li>
-                <h4 className="text- font-bold leading-5 text-white dark:text-white">NIAEFEUP</h4>
-              </li>
-              <li className="text-sm text-gray-300 dark:text-gray-400">
-                <p>Rua Dr. Roberto Frias 4200-465, Porto</p>
-              </li>
-              <li className="text-sm text-gray-400 dark:text-gray-500">
-                <p>Sala B315</p>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="col-span-12 my-auto md:col-span-4">
-          <ul className="flex flex-wrap items-center justify-center gap-x-4 gap-y-2 md:items-end md:justify-end">
-            {socials.map((social, socialIdx) => (
-              <li key={`social-${socialIdx}`}>
-                <a
-                  className="inline-flex items-center justify-center space-x-1 text-sm transition hover:opacity-80"
-                  href={social.url}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <social.icon className="h-5 w-5 text-white dark:text-white" />
-                  <span className="text-white dark:text-white">{social.label}</span>
-                </a>
-              </li>
-            ))}
+    <footer className="bg-gradient-to-t from-gray-800 to-gray-600 px-2 py-4 transition dark:from-dark dark:to-darkish md:px-6 md:py-8">
+      <div className="md:flex md:justify-between">
+        {/* Left */}
+        <div className="flex items-center justify-center space-x-4 md:items-start md:justify-start">
+          <img src={LogoNIAEFEUPImage} alt="NIAEFEUP" className="h-auto w-28" />
+          <ul className="py-2">
+            <li>
+              <h4 className="text-4xl font-bold tracking-tight text-white">NIAEFEUP</h4>
+            </li>
+            <li className="text-lg font-medium text-white">
+              <p>Rua Dr. Roberto Frias 4200-465, Porto</p>
+            </li>
+            <li className="text-lg font-normal text-white">
+              <p>Sala B315</p>
+            </li>
           </ul>
         </div>
+        {/* Right */}
+        <div className="flex flex-col space-y-4">
+          <a className="transition hover:opacity-90" href="https://sigarra.up.pt/feup">
+            <img src={LogoFEUPImage} alt="FEUP" className="h-auto w-48 rounded bg-gray-50 p-1" />
+          </a>
+          <a className="transition hover:opacity-90" href="https://www.aefeup.pt">
+            <img src={LogoAEFEUPImage} alt="AEFEUP" className="h-auto w-48 rounded bg-gray-50 p-2" />
+          </a>
+        </div>
       </div>
-    </div>
+      <hr className="my-6 sm:mx-auto lg:my-8" />
+      <div className="sm:flex sm:items-center sm:justify-between">
+        <ul className="mt-4 flex space-x-4 sm:mt-0 sm:justify-center">
+          {socials.map((social, socialIdx) => (
+            <li key={`social-${socialIdx}`}>
+              <a
+                className="inline-flex items-center justify-center text-sm transition hover:opacity-80"
+                href={social.url}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <social.icon className="h-8 w-8 rounded text-white transition hover:-translate-y-1 hover:opacity-80" />
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="flex flex-col items-center">
+          <p className="text-base font-medium text-white dark:text-gray-300 sm:text-center">
+            © 2022{' '}
+            <a href="https://ni.fe.up.pt" className="hover:underline">
+              NIAEFEUP
+            </a>
+            ™. All Rights Reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
   )
 }
 
--- src/pages/FeupExchange.tsx
+++ src/pages/FeupExchange.tsx
@@ -1,5 +1,5 @@
 const FeupExchangePage = () => (
-  <div className="grid w-full grid-cols-12 gap-x-0 gap-y-8 py-4 px-6 md:px-4 xl:gap-x-6 xl:gap-y-0">
+  <div className="grid w-full grid-cols-12 gap-x-4 gap-y-8 py-4 px-6 md:px-4 xl:gap-x-6 xl:gap-y-0">
     {/* Schedule Preview */}
     <div className="min-h-adjusted order-2 col-span-12 bg-lightest p-3 dark:bg-dark md:order-1 lg:col-span-9">
       <div className="w-full">Request exchange schedule preview content</div>
