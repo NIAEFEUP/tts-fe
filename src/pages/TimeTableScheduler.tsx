bd7e96c677d3f86858c8c603da37fe4f94fac2e3
kiko-g <kikojpgoncalves@gmail.com>
2022-05-21 03:56:25 +0100

    minor styling

--- src/pages/TimeTableScheduler.tsx
+++ src/pages/TimeTableScheduler.tsx
@@ -89,12 +89,8 @@ const TimeTableSchedulerPage = () => {
     setSelected(resolveSelected())
   }, [courses])
 
-  useEffect(() => {
-    // console.log(selected)
-  }, [selected])
-
   return (
-    <div className="grid w-full grid-cols-12 gap-x-0 gap-y-8 py-4 px-6 md:px-4 xl:gap-x-6 xl:gap-y-0">
+    <div className="grid w-full grid-cols-12 gap-x-0 gap-y-8 py-4 px-6 md:px-4 xl:gap-x-0 xl:gap-y-0">
       {/* Schedule Preview */}
       <div className="min-h-adjusted order-2 col-span-12 bg-lightest p-3 dark:bg-dark md:order-1 lg:col-span-9">
         <div className="w-full">Schedule preview content</div>
