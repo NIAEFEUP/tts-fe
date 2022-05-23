912c43c3e19059b6e6b12a23885eeac84142628e
kiko-g <kikojpgoncalves@gmail.com>
2022-05-23 20:44:03 +0100

    extract schedules text

--- src/components/planner/Schedule.tsx
+++ src/components/planner/Schedule.tsx
@@ -8,18 +8,24 @@ type Props = {
 const Schedule = ({ courseOptions }: Props) => {
   return (
     <div className="schedule">
-      {courseOptions.map((courseOption, courseOptionIdx) => (
-        <div key={courseOptionIdx}>
-          <span>{courseOption.course.info.acronym}</span>
-          {courseOption.option ? (
-            <span>
-              : <strong>{courseOption.option.class_name}</strong>
-            </span>
-          ) : null}
-        </div>
-      ))}
+      <SchedulesText courseOptions={courseOptions} />
     </div>
   )
 }
+
+const SchedulesText = ({ courseOptions }: Props) => (
+  <>
+    {courseOptions.map((courseOption, courseOptionIdx) => (
+      <div key={courseOptionIdx}>
+        <span>{courseOption.course.info.acronym}</span>
+        {courseOption.option && (
+          <span>
+            : <strong>{courseOption.option.class_name}</strong>
+          </span>
+        )}
+      </div>
+    ))}
+  </>
+)
 
 export default Schedule
