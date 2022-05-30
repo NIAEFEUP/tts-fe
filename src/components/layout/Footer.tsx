833fee33adfbd3adcc7af049bb0dd393eedc9821
kiko-g <kikojpgoncalves@gmail.com>
2022-05-30 17:39:32 +0100

    update: footer styles

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -21,24 +21,25 @@ const socials: Social[] = [
 const Footer = () => {
   return (
     <footer className="mt-8 bg-gradient-to-t from-gray-800 to-gray-700 px-2 py-4 transition dark:from-dark dark:to-dark md:px-6 md:py-8">
-      <div className="md:flex md:justify-between">
+      <div className="justify-center md:flex md:justify-between">
         {/* Left */}
         <div className="flex items-center justify-center space-x-4 md:items-start md:justify-start">
           <img src={LogoNIAEFEUPImage} alt="NIAEFEUP" className="h-auto w-28" />
-          <ul className="py-2">
+          <ul className="w-min py-2 tracking-tight lg:w-max">
             <li>
-              <h4 className="text-4xl font-bold tracking-tight text-white">NIAEFEUP</h4>
+              <h4 className="text-3xl font-bold text-white lg:text-4xl">NIAEFEUP</h4>
             </li>
-            <li className="text-lg font-medium text-white">
+            <li className="text-base font-medium text-gray-400 lg:text-lg">
               <p>Rua Dr. Roberto Frias 4200-465, Porto</p>
             </li>
-            <li className="text-lg font-normal text-white">
+            <li className="mt-1 text-base font-normal text-gray-400 lg:text-lg">
               <p>Sala B315</p>
             </li>
           </ul>
         </div>
+
         {/* Right */}
-        <div className="flex flex-col space-y-4">
+        <div className="mt-4 flex flex-col items-center space-y-4 md:mt-0">
           <a className="transition hover:opacity-90" href="https://sigarra.up.pt/feup">
             <img src={LogoFEUPImage} alt="FEUP" className="h-auto w-48 rounded bg-gray-50 p-1" />
           </a>
@@ -47,9 +48,11 @@ const Footer = () => {
           </a>
         </div>
       </div>
+
       <hr className="my-6 sm:mx-auto lg:my-8" />
-      <div className="sm:flex sm:items-center sm:justify-between">
-        <ul className="mt-4 flex space-x-4 sm:mt-0 sm:justify-center">
+
+      <div className="flex flex-col items-center justify-between space-y-4 lg:flex-row lg:space-y-0">
+        <ul className="mt-4 flex items-center justify-center space-x-4 sm:mt-0">
           {socials.map((social, socialIdx) => (
             <li key={`social-${socialIdx}`}>
               <a
@@ -63,7 +66,8 @@ const Footer = () => {
             </li>
           ))}
         </ul>
-        <div className="flex flex-col items-center">
+
+        <div className="flex flex-col items-center justify-center">
           <p className="text-base font-medium text-white dark:text-gray-300 sm:text-center">
             © 2022{' '}
             <a href="https://ni.fe.up.pt" className="hover:underline">
