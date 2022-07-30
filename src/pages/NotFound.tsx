fb598d5e68d08c6a1d9a73f4452a90f501165324
André Ávila <avila_andre14@hotmail.com>
2022-07-30 20:00:30 +0100

    Removed comments.

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -9,11 +9,9 @@ const NotFoundPage = () => (
             <Link to="/" className="flex h-auto w-fit items-center justify-center rounded border-2 border-primary bg-primary px-3 py-2 text-xs font-medium text-white transition hover:opacity-80 lg:text-sm xl:px-4">
                 Go to Planner
             </Link>
-        {/* <img className="max-w-lg rounded shadow" src={image} alt="card" /> */}
         </div>
         {/* Mobile */}
         <div className="flex flex-col items-center justify-between space-y-4 py-8 md:hidden">
-        {/* <img className="w-full rounded shadow" src={image} alt="card" /> */}
             <h1 className="text-4xl text-center font-semibold">404</h1>
             <h2 className="text-3xl mb-2 text-center font-medium">PAGE NOT FOUND</h2>
             <Link to="/" className="flex h-auto w-fit items-center justify-center rounded border-2 border-primary bg-primary px-3 py-2 text-xs font-medium text-white transition hover:opacity-80 lg:text-sm xl:px-4">
