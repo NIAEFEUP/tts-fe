b624b07e4d8aeb71d3b81a748ccca4845688c6b0
ZeAntonioM <93012005+ZeAntonioM@users.noreply.github.com>
2022-08-09 10:50:39 +0100

    Done some work on About Page
    Translated to Portuguese some details;
    Worked on text about tts and NIAEFEUP;
    Added two new images to represent each tab.

--- src/images/index.ts
+++ src/images/index.ts
@@ -6,6 +6,8 @@ import HomePageAltImage from './homepage2.jpg'
 import LogoFEDarkImage from './logo-dark.png'
 import LogoFELightImage from './logo-light.png'
 import LogoNIAEFEUPImage from './logo-ni.png'
+import LogoNIAEFEUPAltImage from './logo-ni2.png'
+import ScheduleDarkImage from './schedule-dark.png'
 
 export {
   AppLogo,
@@ -16,4 +18,6 @@ export {
   LogoFEDarkImage,
   LogoFELightImage,
   LogoNIAEFEUPImage,
+  LogoNIAEFEUPAltImage,
+  ScheduleDarkImage,
 }
