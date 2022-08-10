93d197a2965d8ad876ecb32c4a37fab51eec1b60
ZeAntonioM <93012005+ZeAntonioM@users.noreply.github.com>
2022-08-10 15:46:17 +0100

    About page content
    Made some changes to correct syntax;
    Due to the changes in the interface, I've changed the image representing TTS interface with Dark Mode.

--- src/components/home/HeroPlanner.tsx
+++ src/components/home/HeroPlanner.tsx
@@ -13,21 +13,21 @@ const Hero = () => (
       <CardFeature
         title="O que é o tts?"
         subtitle="O teu melhor amigo para escolher e cuidar do horário na FEUP"
-        text="TTS, ou Time Table Selector, é uma ferramenta desenvolvida pelo NIAEFEUP para ajudar os estudantes a construir o seu próprio horário. Se é o teu primeiro ano na FEUP não poderás mudar o teu horário, a FEUP irá atribuir-te um horário e poderás apenas trocar de horário com alguém que tenha interesse. No entanto, a partir do 2º Semestre até ao ultimo Semestre na FEUP terás de construir o teu horário. No entanto, a ferramenta que a Faculdade disponibiliza poderá não ser tão intuitiva e fácil de usar. E para isso nós decidimos criar o TTS, com o intuito de te ajudar, com uma ferramenta de fácil acesso e com uma interface limpa e simples de modo a que não tenhas complicações a construir e gerir o teu horário!"
+        text="TTS, ou Time Table Selector, é uma ferramenta desenvolvida pelo NIAEFEUP para ajudar os estudantes a construir o seu próprio horário. Se é o teu primeiro ano na FEUP não poderás mudar o teu horário, a FEUP irá atribuir-te um horário e poderás apenas trocar de horário com alguém que tenha interesse. No entanto, a partir do 2º Semestre até ao último Semestre na FEUP terás de construir o teu horário. No entanto, a ferramenta que a Faculdade disponibiliza poderá não ser tão intuitiva e fácil de usar. E para isso nós decidimos criar o TTS, com o intuito de ajudar-te, com uma ferramenta de fácil acesso e com uma interface limpa e simples de modo que não tenhas complicações a construir e gerir o teu horário!"
         image={ScheduleDarkImage}
         reverse={false}
       />
       <CardFeature
         title="Porquê usar o tts?"
         subtitle="Porque é a ferramenta ideal que precisas"
-        text="O TTS é uma ferramenta criada para ajudar os estudantes a criar, planear e partilhar os horários antes das inscrições de turmas de forma prática. Assim, podes começar a planear o teu semestre com antecedência, e, algo ainda melhor: Graças às novas features adicionadas, podes fazer o download ou dar upload de qualquer horário. Então tornou-se mais fácil do que nunca conseguires criar um horário e enviar para que os teus amigos fiquem com o mesmo horário que tu e nas mesmas turmas! Para além disso, o TTS tem um algoritmo de colisão de turmas, portanto não precisas de te preocupar em verificar se existe algum problema, o TTS irá advertir-te! "
+        text="O TTS é uma ferramenta criada para ajudar os estudantes a criar, planear e partilhar os horários antes das inscrições de turmas de forma prática. Assim, podes começar a planear o teu semestre com antecedência, e, algo ainda melhor: Graças às novas features adicionadas, podes fazer o download ou dar upload de qualquer horário. Então tornou-se mais fácil do que nunca conseguires criar um horário e enviar para que os teus amigos fiquem com o mesmo horário que tu e nas mesmas turmas! Para além disso, o TTS tem um algoritmo de colisão de turmas, portanto não precisas de preocupar-te em verificar se existe algum problema, o TTS irá advertir-te!"
         image={HomePageAltImage}
         reverse={true}
       /> 
       <CardFeature
         title="Novas features do TTS e futuras atualizações"
         subtitle="Temos algumas novidades nesta nova versão do TTS"
-        text="Desde o dark mode até ao download/upload dos horários, temos algumas novidades nesta release. Como já foi apresentado, criamos um método que permite que faças o teu horário e o descarregues de modo a que o possas ter no teu computador e que possas enviar a qualquer amigo ou até dar upload para quando voltares ao TTS. Podes também exportar o horário para csv para poderes preencher no Sigarra! Adicionamos também o Dark Mode para aumentar a acessibilidade dos estudantes. Pretendemos no futuro adicionar abas para poderes criar mais do que um horário ao mesmo tempo e poderes comparar e planear ainda melhor! Para além disso, num futuro breve iremos lançar também o FEUPExchange, que será ainda melhor para te ajudar a trocar de turma. Vemo-nos em breve!"
+        text="Desde o Dark Mode até ao download/upload dos horários, temos algumas novidades nesta release. Como já foi apresentado, criamos um método que permite que faças o teu horário e o descarregues de modo que o possas ter no teu computador e que consigas enviar a qualquer amigo ou até dar upload para quando voltares ao TTS. Podes também exportar o horário para CSV para poderes preencher no Sigarra! Adicionamos também o Dark Mode para aumentar a acessibilidade dos estudantes. Por fim, criamos opções de horários de modo que possas planear e testar vários horários em simultâneo! Pretendemos num futuro breve lançar também o FEUPExchange, que será ainda melhor para te ajudar a trocar de turma. Vemo-nos em breve!"
         image={HomePageImage}
         reverse={false}
       />
