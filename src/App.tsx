6d20c9974a5eb47d74379408ae01af14e02bf9fa
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2022-08-15 15:50:49 +0100

    update: paths with tts prefix

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,17 @@ import { AboutPage, ProfilePage, FeupExchangePage, TimeTableSchedulerPage, NotFo
 import './app.css'
 
 const pages = [
-  { path: '/about', location: 'About', element: AboutPage, liquid: true },
-  { path: '/profile', location: 'Profile', element: ProfilePage, liquid: true },
-  { path: '/tts', location: 'Planner', element: TimeTableSchedulerPage, liquid: true },
-  { path: '/exchange', location: 'Exchange', element: FeupExchangePage, liquid: true },
-  { path: '/faqs', location: 'FAQs', element: FaqsPage, liquid: true },
-  { path: '/*', location: 'NotFound', element: NotFoundPage, liquid: true },
+  { path: '/tts/about', location: 'About', element: AboutPage, liquid: true },
+  { path: '/tts/profile', location: 'Profile', element: ProfilePage, liquid: true },
+  { path: '/tts/planner', location: 'Planner', element: TimeTableSchedulerPage, liquid: true },
+  { path: '/tts/exchange', location: 'Exchange', element: FeupExchangePage, liquid: true },
+  { path: '/tts/faqs', location: 'FAQs', element: FaqsPage, liquid: true },
+  { path: '/tts/*', location: 'NotFound', element: NotFoundPage, liquid: true },
 ]
 
 const redirects = [
-  { from: '/', to: '/tts' },
-  { from: '/home', to: '/about' },
+  { from: '/', to: '/tts/planner' },
+  { from: '/tts/home', to: '/tts/about' },
 ]
 
 const App = () => (
