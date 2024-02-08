1794fc3f861875b9f3a07bc4f3c4fa819c5a8d51
diogotvf7 <diogotvf7@gmail.com>
2024-02-08 15:51:07 +0000

    Updated courses order to mimic sigarra

--- src/components/planner/sidebar/CoursesController.tsx
+++ src/components/planner/sidebar/CoursesController.tsx
@@ -8,16 +8,16 @@ const CoursesController = ({ multilpleOptionsHook, isImportedOptionHook }) => {
   return (
     <div className="flex w-full flex-col gap-4 px-0 py-2">
       {multipleOptions.selected.length > 0 &&
-        removeDuplicatesFromCourseOption(multipleOptions.options[multipleOptions.index]).map(
-          (courseOption, courseOptionIdx) => (
+        removeDuplicatesFromCourseOption(multipleOptions.options[multipleOptions.index])
+          .sort((a, b) => a.course.info.sigarra_id - b.course.info.sigarra_id)
+          .map((courseOption, courseOptionIdx) => (
             <ScheduleListbox
               courseOption={courseOption}
               multipleOptionsHook={[multipleOptions, setMultipleOptions]}
               isImportedOptionHook={[isImportedOption, setIsImportedOption]}
               key={`course-schedule-listbox-${multipleOptions.index}-${courseOption.course.info.id}`}
             />
-          )
-        )}
+          ))}
     </div>
   )
 }
