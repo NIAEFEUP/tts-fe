a6a146ab263b1e34218dd14d93b80764c3b78123
Tomás Palma <up202108880@edu.fe.up.pt>
2024-02-18 17:31:53 +0000

    feat: add route to exchange page
    Co-authored-by: Pedro Oliveira <pedro.oliveira2003g@gmail.com>

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,5 +2,6 @@ import AboutPage from './About'
 import TimeTableSchedulerPage from './TimeTableScheduler'
 import NotFoundPage from './NotFound'
 import FaqsPage from './Faqs'
+import ExchangePage from './Exchange'
 
-export { AboutPage, TimeTableSchedulerPage, NotFoundPage, FaqsPage }
+export { AboutPage, TimeTableSchedulerPage, NotFoundPage, FaqsPage, ExchangePage }
