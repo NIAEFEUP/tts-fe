a97dd4ba0e1d1b483bb0a06e0347d808ab374834
pfpo <pedro.oliveira2003g@gmail.com>
2024-02-23 08:48:39 +0000

    Select and input for direct exchange (broken)
    Co-authored-by: joaonevesf <joaodnfernandes99@gmail.com>

--- src/components/exchange/DirectExchangeSelection.tsx
+++ src/components/exchange/DirectExchangeSelection.tsx
@@ -1,18 +1,48 @@
-import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import { ArrowRightIcon } from '@heroicons/react/24/outline'
+import { Input } from '../ui/input'
+import { Select } from '../ui/select'
+import { SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@radix-ui/react-select'
 
 export function DirectExchangeSelection(props) {
-    return (
-        <div className="flex justify-center">
-            <div className="flex flex-col space-y-4 mt-4">
-                <p>{props.UC}</p>
-                <div className="flex flex-row items-center">
-                    <span className="text-center bg-slate-200 w-fit p-2">{props.Class}</span>
-                    <span><ArrowRightIcon className="h-5 w-5 mx-2"></ArrowRightIcon></span>
-                </div>
-            </div>
-            <div className="flex flex-col space-y-4 mt-4">
-                <p>Estudante</p>
-            </div>
+  return (
+    <div className="flex w-full justify-between">
+      <div className="mt-4 flex flex-col space-y-4">
+        <p>{props.UC}</p>
+        <div className="flex flex-row items-center">
+          <span className="w-fit bg-slate-200 p-2 text-center">{props.Class}</span>
+          <span>
+            <ArrowRightIcon className="mx-2 h-5 w-5"></ArrowRightIcon>
+          </span>
+          <Select>
+            <SelectTrigger className="w-[90px]">
+              <SelectValue placeholder="Select a class" />
+            </SelectTrigger>
+            <SelectContent className="flex flex-row">
+              <SelectGroup>
+                <SelectItem value="3LEIC01">3LEIC01</SelectItem>
+                <SelectItem value="3LEIC01">3LEIC01</SelectItem>
+                <SelectItem value="3LEIC01">3LEIC01</SelectItem>
+                <SelectItem value="3LEIC01">3LEIC01</SelectItem>
+                <SelectItem value="3LEIC01">3LEIC01</SelectItem>
+                <SelectItem value="3LEIC01">3LEIC01</SelectItem>
+                <SelectItem value="3LEIC01">3LEIC01</SelectItem>
+                <SelectItem value="3LEIC01">3LEIC01</SelectItem>
+                <SelectItem value="3LEIC01">3LEIC01</SelectItem>
+                <SelectItem value="3LEIC01">3LEIC01</SelectItem>
+                <SelectItem value="3LEIC01">3LEIC01</SelectItem>
+                <SelectItem value="3LEIC01">3LEIC01</SelectItem>
+                <SelectItem value="3LEIC01">3LEIC01</SelectItem>
+                <SelectItem value="3LEIC01">3LEIC01</SelectItem>
+                <SelectItem value="3LEIC01">3LEIC01</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
         </div>
-    );
+      </div>
+      <div className="mt-4 flex flex-col space-y-4">
+        <p>Estudante</p>
+        <Input></Input>
+      </div>
+    </div>
+  )
 }
