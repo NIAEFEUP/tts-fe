ad2675ffedf7263ca7e20561b3fec7b067412ad8
rubuy-74 <rubemviscard2635@gmail.com>
2024-02-27 17:07:07 +0000

    Reorganize schedule selector in mobile mode

--- src/pages/TimeTableScheduler.tsx
+++ src/pages/TimeTableScheduler.tsx
@@ -398,7 +398,7 @@ const TimeTableSchedulerPage = () => {
   return (
     <div className="grid w-full grid-cols-12 gap-x-4 gap-y-4 px-4 py-4">
       {/* Schedule Preview */}
-      <div className="lg:min-h-adjusted order-1 col-span-12 min-h-min rounded bg-lightest px-3 py-3 dark:bg-dark lg:col-span-9 2xl:px-5 2xl:py-5">
+      <div className="lg:min-h-adjusted order-3 lg:order-1 col-span-12 min-h-min rounded bg-lightest px-3 py-3 dark:bg-dark lg:col-span-9 2xl:px-5 2xl:py-5">
         <div className="h-full w-full">
           <Schedule courseOptions={multipleOptions.selected} />
         </div>
