f071848a5853f06986a8166288abedcf4da4b715
Wallen Ribeiro <up202109260@edu.fe.up.pt>
2024-02-27 01:59:05 +0000

    creating pr so that i can emulate deployed version

--- src/pages/Faqs.tsx
+++ src/pages/Faqs.tsx
@@ -6,6 +6,7 @@ const FaqsPage = () => {
     if (!window.location.href.split('#')[1]) document.getElementById('layout').scrollIntoView()
   }, [])
 
+  
   return (
     <div className="container mx-auto w-full max-w-7xl space-y-4 px-4 py-6 md:py-10 md:px-6">
       <HeaderFaqs />
