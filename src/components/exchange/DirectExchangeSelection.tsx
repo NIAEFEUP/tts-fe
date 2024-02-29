179174e6462c7cee5a95a92f9dd0ea69db14eeed
pfpo <pedro.oliveira2003g@gmail.com>
2024-02-29 20:09:51 +0000

    removed comment

--- src/components/exchange/DirectExchangeSelection.tsx
+++ src/components/exchange/DirectExchangeSelection.tsx
@@ -87,12 +87,6 @@ export function DirectExchangeSelection(props) {
                                                 setOpen(false)
                                             }}
                                         >
-                                            {/*<Check
-                                                className={cn(
-                                                    "mr-2 h-4 w-4",
-                                                    value === framework.value ? "opacity-100" : "opacity-0"
-                                                )}
-                                            />*/}
                                             {ucClass.label}
                                         </CommandItem>
                                     ))}
