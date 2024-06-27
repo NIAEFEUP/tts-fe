b253b987593b065fbd2bbf4caf89a34b19f568d3
Process-ing <rbruno.p.o@gmail.com>
2024-06-27 21:41:06 +0100

    Integrate MultipleOptions in CombinedProvider

--- src/contexts/CombinedProvider.tsx
+++ src/contexts/CombinedProvider.tsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
+import { MultipleOptions } from "../@types/new_index";
 import { useDarkMode } from "../hooks";
 import { ThemeContext } from "./ThemeContext";
+import MultipleOptionsContext from "./MultipleOptionsContext";
 
 const CombinedProvider = ({ children }) => {
     const [enabled, setEnabled] = useDarkMode()  // TODO: Stop using a hook here (who smoked here?)
+    const [multipleOptions, setMultipleOptions] = useState<MultipleOptions>(null);
+    const [selectedOption, setSelectedOption] = useState<number>(0);
 
     return (
         <ThemeContext.Provider value={{ enabled, setEnabled }}>
-            {children}
+            <MultipleOptionsContext.Provider value={{ multipleOptions, setMultipleOptions, selectedOption, setSelectedOption }}>
+                {children}
+            </MultipleOptionsContext.Provider>
         </ThemeContext.Provider>
     );
 };
