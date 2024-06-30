16efeeb4b37ca3af32f6a3abc46255ebce5e3c91
Process-ing <rbruno.p.o@gmail.com>
2024-06-30 18:42:34 +0100

    Use StorageAPI functions for selected option in CombinedProvider

--- src/contexts/CombinedProvider.tsx
+++ src/contexts/CombinedProvider.tsx
@@ -7,12 +7,17 @@ import StorageAPI from "../api/storage";
 
 const CombinedProvider = ({ children }) => {
   const [enabled, setEnabled] = useDarkMode()  // TODO (Process-ing): Stop using a hook (who smoked here?)
-  const [multipleOptions, setMultipleOptionsState] = useState<MultipleOptions>(StorageAPI.getMultipleOptionsStorage());
-  const [selectedOption, setSelectedOption] = useState<number>(0);
+  const [multipleOptions, setMultipleOptionsState] = useState<MultipleOptions>(StorageAPI.getMultipleOptionsStorage());  // TODO (Process-ing): Investigate integrating localStorage directly in the state (like in useLocalStorage)
+  const [selectedOption, setSelectedOptionState] = useState<number>(StorageAPI.getSelectedOptionStorage());
 
   const setMultipleOptions = (options: MultipleOptions) => {
     setMultipleOptionsState(options);
     StorageAPI.setMultipleOptionsStorage(options);
+  }
+
+  const setSelectedOption = (option: number) => {
+    setSelectedOptionState(option);
+    StorageAPI.setSelectedOptionStorage(option);
   }
 
   return (
