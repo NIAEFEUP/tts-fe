5d9e963ccc4a6d2d8b53a70796196fc1ed40d160
Wallen-Ribeiro <up202109260@edu.fe.up.pt>
2024-08-27 16:07:40 +0100

    added protobuf plugin for typescript

---
+++ src/generated/x_pb_service.js
@@ -0,0 +1,61 @@
+// package: example
+// file: x.proto
+
+var x_pb = require("./x_pb");
+var grpc = require("@improbable-eng/grpc-web").grpc;
+
+var Greeter = (function () {
+  function Greeter() {}
+  Greeter.serviceName = "example.Greeter";
+  return Greeter;
+}());
+
+Greeter.SayHello = {
+  methodName: "SayHello",
+  service: Greeter,
+  requestStream: false,
+  responseStream: false,
+  requestType: x_pb.HelloRequest,
+  responseType: x_pb.HelloResponse
+};
+
+exports.Greeter = Greeter;
+
+function GreeterClient(serviceHost, options) {
+  this.serviceHost = serviceHost;
+  this.options = options || {};
+}
+
+GreeterClient.prototype.sayHello = function sayHello(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(Greeter.SayHello, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+exports.GreeterClient = GreeterClient;
+
