6c9744cc95c3132976722c1671cb4cb90f307455
Wallen-Ribeiro <up202109260@edu.fe.up.pt>
2024-12-26 13:58:32 +0000

    restreicted access to admin page

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -2,12 +2,21 @@ import { BrowserRouter, Route, Router } from "react-router-dom";
 import { AdminMainContent } from "../components/admin/AdminMainContent";
 import { AdminSidebar } from "../components/admin/AdminSidebar";
 import { AdminSettings } from "../components/admin/AdminSettings";
+import SessionContext from '../contexts/SessionContext'
+import { useContext } from "react";
+
 
 type Props = {
     page: string;
 }
 
 const AdminPage = ({ page }: Props) => {
+    const {signedIn, user} = useContext(SessionContext);
+
+    if (!signedIn || user?.role !== 'admin') {
+        return <div>Access Denied. You do not have admin credentials.</div>;
+    }
+    
     return (
         <div className="flex flex-row h-screen gap-x-2">
             <div className="w-2/12">
