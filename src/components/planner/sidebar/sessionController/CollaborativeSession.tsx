021013d8cad73b5746ff088ae8cdf2ee18ee5688
racoelhosilva <racoelhosilva@gmail.com>
2025-02-03 10:39:31 +0000

    feat: added number of participants in current session indicator

--- src/components/planner/sidebar/sessionController/CollaborativeSession.tsx
+++ src/components/planner/sidebar/sessionController/CollaborativeSession.tsx
@@ -23,11 +23,18 @@ const CollaborativeSession = () => {
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
 
+  const participants = sessions.find((session) => session.id === currentSessionId)?.participants.length;
+
   return (
     <CollabSessionContext.Provider value={{ sessions, setSessions, currentSessionId, setCurrentSessionId }}>
       <div>
-        <Button variant="icon" className={!currentSessionId ? "bg-primary" : "bg-green-600"} onClick={openModal}>
+        <Button variant="icon" className={`${!currentSessionId ? "bg-primary" : "bg-green-600"} relative`} onClick={openModal}>
           <UsersIcon className="h-5 w-5" />
+          {currentSessionId && participants > 0 && (
+            <span className="absolute bottom-0.5 right-0.5 bg-primary text-white text-xs font-bold px-1.5 py-0.5 rounded-full">
+              {participants}
+            </span>
+          )}
         </Button>
 
         <CollabModal isOpen={isModalOpen} closeModal={closeModal} />
