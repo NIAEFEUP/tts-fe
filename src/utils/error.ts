ff670a4ac064a81d3767eb3d2eb060468a962c86
Tomás Palma <up202108880@edu.fe.up.pt>
2025-02-04 10:36:31 +0000

    fix: better conflict text to indicate conflicts may not happen with us

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,5 +1,5 @@
 export const exchangeErrorToText = {
-    "classes-overlap": "Ao realizar esta troca, terias conflitos.",
+    "classes-overlap": "Ao realizar esta troca, haveriam conflitos para alguma pessoa envolvida na troca.",
     "students-with-incorrect-classes": "Alguma das pessoas envolvidas não está numa das turmas incluídas na troca.",
     "duplicate-request": "Já existe um pedido de troca igual a este.",
 }
