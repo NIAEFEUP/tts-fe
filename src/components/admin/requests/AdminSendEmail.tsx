458b6dda3edaf05412b5ee161ea04341eb6391d3
Tomás Palma <up202108880@edu.fe.up.pt>
2025-02-09 13:02:11 +0000

    open email in new tab for admins

--- src/components/admin/requests/AdminSendEmail.tsx
+++ src/components/admin/requests/AdminSendEmail.tsx
@@ -16,7 +16,11 @@ export const AdminSendEmail = ({
     message=""
 }: Props) => {
     return <>
-        <a href={`${mailtoStringBuilder(nmec)}?subject=${subject}&cc=inscricoes.turmas.leic@fe.up.pt&body=Viva,%0D%0A%0D%0A ${message} %0D%0A%0D%0ACmpts,%0D%0ADaniel Silva%0D%0A(pela comissão de inscrição em turmas)`}>
+        <a 
+            target="_blank"
+            rel="noopener noreferrer"
+            href={`${mailtoStringBuilder(nmec)}?subject=${subject}&cc=inscricoes.turmas.leic@fe.up.pt&body=Viva,%0D%0A%0D%0A ${message} %0D%0A%0D%0ACmpts,%0D%0ADaniel Silva%0D%0A(pela comissão de inscrição em turmas)`}
+        >
             <Button
                 variant="secondary"
             >
