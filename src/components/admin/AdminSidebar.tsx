bbe2c6320ad92e2decf49f29689721a4be030dbc
Mariana Almeida <marianacabralmeida@gmail.com>
2025-08-21 00:13:05 +0100

    Logout button in the admin area working

--- src/components/admin/AdminSidebar.tsx
+++ src/components/admin/AdminSidebar.tsx
@@ -1,9 +1,26 @@
-
+import { useNavigate } from "react-router-dom"; 
 import { Separator } from "../ui/separator"
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton } from "../ui/sidebar"
 import { ArrowLeftEndOnRectangleIcon, RectangleGroupIcon, PaperAirplaneIcon } from "@heroicons/react/24/outline"
+import { useContext, useState } from "react";
+import SessionContext from "../../contexts/SessionContext";
+import ScheduleContext from "../../contexts/ScheduleContext";
+import authService from "../../api/services/authService";
 
 export const AdminSidebar = () => {
+  const [loggingOut, setLoggingOut] = useState(false);
+  const navigate = useNavigate(); 
+
+  const { user, setSignedIn } = useContext(SessionContext);
+  const { setExchangeSchedule } = useContext(ScheduleContext);
+
+  const logout = async () => {
+    setLoggingOut(true);
+    setExchangeSchedule([]);
+    await authService.logout(user.token, setSignedIn, setLoggingOut);
+    navigate("/");
+  };
+
     return (
         <Sidebar className="bg-white">
             <SidebarHeader className="flex flex-row gap-2 p-4">
@@ -19,20 +36,19 @@ export const AdminSidebar = () => {
                             <span>Pedidos</span>
                         </a>
                     </SidebarMenuButton>
-                    {/* <SidebarMenuButton asChild>
-                        <a href="/admin/settings">
-                            <AdjustmentsHorizontalIcon className="w-6 h-6" />
-                            <span>Definições</span>
-                        </a>
-                    </SidebarMenuButton> */}
                 </SidebarMenu>
             </SidebarContent>
             <SidebarFooter>
                 <SidebarMenuButton asChild>
-                    <a href="/admin">
+                    <button
+                        type="button"
+                        className="w-full flex flex-row justify-center gap-2 items-center"
+                        onClick={logout}
+                        disabled={loggingOut}
+                >
                         <ArrowLeftEndOnRectangleIcon className="w-6 h-6" />
-                        <span>Terminar Sessão</span>
-                    </a>
+                        {loggingOut ? <span>A sair...</span> : <span>Terminar Sessão</span>}
+                    </button>
                 </SidebarMenuButton>
             </SidebarFooter>
         </Sidebar>
