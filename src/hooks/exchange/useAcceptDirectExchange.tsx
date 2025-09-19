d2300d0c786830ee304f94b4f6ca2f70dd0d4e99
Tomás Palma <up202108880@edu.fe.up.pt>
2025-09-19 17:02:24 +0100

    fix: recebidos tab crashing

--- src/hooks/exchange/useAcceptDirectExchange.tsx
+++ src/hooks/exchange/useAcceptDirectExchange.tsx
@@ -2,20 +2,21 @@ import useSWRMutation from "swr/mutation";
 import exchangeRequestService from "../../api/services/exchangeRequestService";
 
 export default (id: number) => {
-    const submit = async () => {
-       return await exchangeRequestService.acceptDirectExchangeRequest(id);
-    }
+  const submit = async () => {
+    if (!id) return;
+    return await exchangeRequestService.acceptDirectExchangeRequest(id);
+  }
 
-    const { error, trigger, isMutating } = useSWRMutation(
-        `${id}`,
-        submit
-    );
+  const { error, trigger, isMutating } = useSWRMutation(
+    `${id}`,
+    submit
+  );
 
-    return {
-        error,
-        trigger,
-        isMutating
-    };
+  return {
+    error,
+    trigger,
+    isMutating
+  };
 };
 
 
