730d75ea458b3d1f7147a86e87f61f8dbcc65c95
Ana Alves <anafalves999@gmail.com>
2026-01-23 21:28:42 +0000

    removed badge component

--- src/components/planner/ScheduleType.tsx
+++ src/components/planner/ScheduleType.tsx
@@ -8,47 +8,52 @@ type Props = {
 }
 
 const ScheduleTypes = ({ types, hiddenLessonsTypes, setHiddenLessonsTypes }: Props) => {
-  const toggleType = (lessonType: string) => {
-    if (hiddenLessonsTypes.includes(lessonType)) {
-      setHiddenLessonsTypes(hiddenLessonsTypes.filter(t => t !== lessonType))
-    } else {
-      setHiddenLessonsTypes([...hiddenLessonsTypes, lessonType])
-    }
-  }
   return (
     <>
-      <div className="flex flex-wrap gap-2">
-        {types.map((lessonType) => {
-          const isHidden = hiddenLessonsTypes.includes(lessonType)
+    <div className="flex flex-wrap gap-4 items-center"></div>
+      {types.map((lessonType) => {
+        const isHidden = hiddenLessonsTypes.includes(lessonType)
+        return (
+          <label
+            className="group relative flex items-center gap-1.5 overflow-x-hidden rounded-lg hover:cursor-pointer lg:gap-1"
+            key={`lesson-type-${lessonType}`}
+          >
+            <input
+              type="checkbox"
+              className="peer hidden"
+              checked={isHidden}
+              onChange={(e) => {
+                if (e.target.checked) {
+                  setHiddenLessonsTypes([...hiddenLessonsTypes, lessonType])
+                } else {
+                  setHiddenLessonsTypes(hiddenLessonsTypes.filter((type) => type !== lessonType))
+                }
+              }}
+            />
 
-          return (
-            <Badge
-              key={lessonType}
-              onClick={() => toggleType(lessonType)}
-              variant={isHidden ? 'outline' : 'secondary'}
+            <span
               className={`
-                cursor-pointer select-none transition-all
-                hover:opacity-90
+                flex h-4 w-4 items-center justify-center rounded
+                ${'bg-schedule-' + lessonType.toLowerCase() + '/80'}
               `}
             >
-              <span
-                className={`
-                  flex h-4 w-4 items-center justify-center rounded mr-2
-                  ${'bg-schedule-' + lessonType.toLowerCase() + '/80'}
-                  peer-checked:opacity-50
-                `}
-              >
-                {!isHidden &&    
-                (<span className="absolute text-white text-[11px] font-semibold leading-none">
-                  ✓
-                </span>)
-                }
-              </span>
+              {!isHidden &&    
+              (<span className="absolute text-white text-[11px] font-semibold leading-none">
+                ✓
+              </span>)
+              }
+            </span>
+
+        
+            <span >
               {getClassType(lessonType)}
-            </Badge>
-          )
-        })}
-      </div>
+            </span>
+
+            <div className="z-5 absolute -inset-full top-0 block h-full w-1/2 -skew-x-12 transform bg-gradient-to-r from-transparent to-white opacity-40 group-hover:animate-shine" />
+         
+          </label>
+        )
+      })}
     </>
   )
 }
