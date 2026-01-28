a777e43c36c66ce26b590fc334e3bf8c98602ad2
Ana Alves <anafalves999@gmail.com>
2026-01-28 20:38:54 +0000

    code improvement of SchedulesType

--- src/components/planner/ScheduleType.tsx
+++ src/components/planner/ScheduleType.tsx
@@ -7,27 +7,32 @@ type Props = {
 }
 
 const ScheduleTypes = ({ types, hiddenLessonsTypes, setHiddenLessonsTypes }: Props) => {
+  
+  const handleToggle = (lessonType: string) => {
+    if (hiddenLessonsTypes.includes(lessonType)) {
+      setHiddenLessonsTypes(
+        hiddenLessonsTypes.filter((type) => type !== lessonType)
+      )
+    } else {
+      setHiddenLessonsTypes([...hiddenLessonsTypes, lessonType])
+    }
+  }
+  
   return (
     <>
-    <div className="flex flex-wrap gap-4 items-center"></div>
       {types.map((lessonType) => {
         const isHidden = hiddenLessonsTypes.includes(lessonType)
+        
         return (
           <label
             className="group relative flex items-center gap-1.5 overflow-x-hidden rounded-lg hover:cursor-pointer lg:gap-1"
-            key={`lesson-type-${lessonType}`}
+            key={lessonType}
           >
             <input
               type="checkbox"
               className="peer hidden"
               checked={isHidden}
-              onChange={(e) => {
-                if (e.target.checked) {
-                  setHiddenLessonsTypes([...hiddenLessonsTypes, lessonType])
-                } else {
-                  setHiddenLessonsTypes(hiddenLessonsTypes.filter((type) => type !== lessonType))
-                }
-              }}
+              onChange={() => handleToggle(lessonType)}
             />
 
             <span
@@ -36,20 +41,16 @@ const ScheduleTypes = ({ types, hiddenLessonsTypes, setHiddenLessonsTypes }: Pro
                 ${'bg-schedule-' + lessonType.toLowerCase() + '/80'}
               `}
             >
-              {!isHidden &&    
-              (<span className="absolute text-white text-[11px] font-semibold leading-none">
+              {!isHidden && (
+                <span className="absolute text-white text-[11px] font-semibold leading-none">
                 ✓
-              </span>)
-              }
+                </span>
+              )}
             </span>
 
-        
-            <span >
-              {getClassType(lessonType)}
-            </span>
-
+            {getClassType(lessonType)}
+          
             <div className="z-5 absolute -inset-full top-0 block h-full w-1/2 -skew-x-12 transform bg-gradient-to-r from-transparent to-white opacity-40 group-hover:animate-shine" />
-         
           </label>
         )
       })}
