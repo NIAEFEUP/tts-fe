971db098e834b330eae2a43084c1c9da89a72f18
Jose Sousa <2409jmsousa@gmail.com>
2026-02-03 22:52:00 +0000

    feat: changed uniqueMetadata to be actually unique

--- src/components/admin/requests/cards/TreatExchangeButton.tsx
+++ src/components/admin/requests/cards/TreatExchangeButton.tsx
@@ -15,9 +15,15 @@ export const TreatExchangeButton = ({
     const { studentCourseMetadata } = useStudentCourseMetadata(nmec, courseId);
 
     const uniqueMetadata = studentCourseMetadata
-        ? Array.from(studentCourseMetadata.values()).filter(metadata => metadata.length > 0).flat()
+        ? Array.from(studentCourseMetadata.values())
+            .filter(metadata => metadata.length > 0)
+            .flat()
+            .filter(
+                (metadata, index, self) =>
+                    index === self.findIndex(m => m.fest_id === metadata.fest_id && m.course.id === metadata.course.id)
+            )
         : [];
-
+        
     return (
         <>
             {
