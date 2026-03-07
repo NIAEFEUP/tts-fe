a964372b39153b7a5f9d0c570f52566ebdb230e7
Pedro Lunet <pedrolunet@gmail.com>
2026-03-07 19:02:03 +0000

    format sidebar

--- src/components/planner/Sidebar.tsx
+++ src/components/planner/Sidebar.tsx
@@ -12,42 +12,41 @@ import { Button } from '../ui/button'
  * Sidebar with all the main schedule interactions
  */
 const Sidebar = () => {
-  const { multipleOptions, selectedOption, setMultipleOptions } = useContext(MultipleOptionsContext);
-  const { toggleSidebarPosition } = useSidebarContext();
-  
+  const { multipleOptions, selectedOption, setMultipleOptions } = useContext(MultipleOptionsContext)
+  const { toggleSidebarPosition } = useSidebarContext()
+
   const noClassesPicked = !multipleOptions[selectedOption]?.course_options.some(
-    (option) => option.picked_class_id !== null
-  );
+    (option) => option.picked_class_id !== null,
+  )
 
   const eraseClasses = () => {
-    const currentOption = multipleOptions[selectedOption];
-  
-    const updatedCourseOptions = currentOption.course_options.map(courseOption => ({
+    const currentOption = multipleOptions[selectedOption]
+
+    const updatedCourseOptions = currentOption.course_options.map((courseOption) => ({
       ...courseOption,
       picked_class_id: null,
       locked: false,
-    }));
-  
-    const updatedMultipleOptions = [...multipleOptions];
+    }))
+
+    const updatedMultipleOptions = [...multipleOptions]
     updatedMultipleOptions[selectedOption] = {
       ...currentOption,
       course_options: updatedCourseOptions,
-    };
-  
-    setMultipleOptions(updatedMultipleOptions);
-  };
+    }
 
+    setMultipleOptions(updatedMultipleOptions)
+  }
 
   return (
-    <div className="order-2 col-span-12 flex flex-col justify-between rounded-md bg-lightest px-3 py-3 dark:bg-dark lg:col-span-3 2xl:px-4 2xl:py-4
-                h-[85vh] overflow-y-auto">
-        <div className="space-y-1">
-          <SessionController />
-          <OptionsController />
-          <SelectedOptionController
-            currentOption={multipleOptions[selectedOption].course_options}
-          />
-          <CoursesController />
+    <div
+      className="order-2 col-span-12 flex flex-col justify-between rounded-md bg-lightest px-3 py-3 dark:bg-dark lg:col-span-3 2xl:px-4 2xl:py-4
+                h-[85vh] overflow-y-auto"
+    >
+      <div className="space-y-1">
+        <SessionController />
+        <OptionsController />
+        <SelectedOptionController currentOption={multipleOptions[selectedOption].course_options} />
+        <CoursesController />
       </div>
       <footer className=" gap-x-1 border-white-300 text-center flex items-end justify-end">
         <Button
@@ -58,7 +57,8 @@ const Sidebar = () => {
           <TrashIcon className="h-5 w-5" />
           <span>Limpar</span>
         </Button>
-        <button title='Mudar o lado da Sidebar'
+        <button
+          title="Mudar o lado da Sidebar"
           onClick={toggleSidebarPosition}
           className="hidden md:flex items-center justify-center gap-2 w-[48px] h-[40px] bg-primary hover:opacity-80 dark:text-white rounded-md p-1 text-gray text-sm"
         >
