d0f05741c7a5fb961dc840b3b8b6000971b5fd98
Pedro Lunet <pedrolunet@gmail.com>
2026-03-08 18:59:03 +0000

    updataed checkbox usage on schedule type

--- src/components/planner/ScheduleType.tsx
+++ src/components/planner/ScheduleType.tsx
@@ -1,18 +1,17 @@
+import React from 'react'
 import { getClassType } from '../../utils'
+import { Checkbox } from '../ui/new/checkbox'
 
 type Props = {
-  types: string[],
-  hiddenLessonsTypes: string[],
+  types: string[]
+  hiddenLessonsTypes: string[]
   setHiddenLessonsTypes: (hiddenLessonsTypes: string[]) => void
 }
 
 const ScheduleTypes = ({ types, hiddenLessonsTypes, setHiddenLessonsTypes }: Props) => {
-  
   const handleToggle = (lessonType: string) => {
     if (hiddenLessonsTypes.includes(lessonType)) {
-      setHiddenLessonsTypes(
-        hiddenLessonsTypes.filter((type) => type !== lessonType)
-      )
+      setHiddenLessonsTypes(hiddenLessonsTypes.filter((type) => type !== lessonType))
     } else {
       setHiddenLessonsTypes([...hiddenLessonsTypes, lessonType])
     }
@@ -22,36 +21,27 @@ const ScheduleTypes = ({ types, hiddenLessonsTypes, setHiddenLessonsTypes }: Pro
     <>
       {types.map((lessonType) => {
         const isHidden = hiddenLessonsTypes.includes(lessonType)
-        
+
         return (
-          <label
-            className="group relative flex items-center gap-1.5 overflow-x-hidden rounded-lg hover:cursor-pointer lg:gap-1"
+          <div
+            className="group relative flex items-center gap-1.5 overflow-x-hidden rounded-lg px-1 py-0.5 lg:gap-1"
             key={lessonType}
           >
-            <input
-              type="checkbox"
-              className="peer hidden"
-              checked={isHidden}
+            <Checkbox
+              checked={!isHidden}
               onChange={() => handleToggle(lessonType)}
+              style={
+                {
+                  '--color-accent': `var(--color-schedule-${lessonType.toLowerCase()})`,
+                  '--color-accent-foreground': '#fff',
+                } as React.CSSProperties
+              }
             />
 
-            <span
-              className={`
-                flex h-4 w-4 items-center justify-center rounded
-                ${'bg-schedule-' + lessonType.toLowerCase() + '/80'}
-              `}
-            >
-              {!isHidden && (
-                <span className="absolute text-white text-[11px] font-semibold leading-none">
-                ✓
-                </span>
-              )}
-            </span>
-
             {getClassType(lessonType)}
-          
+
             <div className="z-5 absolute -inset-full top-0 block h-full w-1/2 -skew-x-12 transform bg-linear-to-r from-transparent to-white opacity-40 group-hover:animate-shine" />
-          </label>
+          </div>
         )
       })}
     </>
