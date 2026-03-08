6fafedc374d2dd9cc4396017e7460b6edcaf47c6
Pedro Lunet <pedrolunet@gmail.com>
2026-03-08 19:07:09 +0000

    updated icons, modal and button. usage on collab modal

--- src/components/planner/sidebar/sessionController/CollabPickSession.tsx
+++ src/components/planner/sidebar/sessionController/CollabPickSession.tsx
@@ -1,10 +1,10 @@
-import { PlayCircleIcon, UserGroupIcon } from '@heroicons/react/20/solid';
-import { Button } from '../../../ui/button';
-import { CollabSession } from '../../../../@types';
-import toHumanReadableTimeDiff from '../../../../utils/human-time';
+import { PlayCircle, Users } from 'lucide-react'
+import { Button } from '../../../ui/new/newButton'
+import { CollabSession } from '../../../../@types'
+import toHumanReadableTimeDiff from '../../../../utils/human-time'
 
 type Props = {
-  sessions: Array<CollabSession>,
+  sessions: Array<CollabSession>
   onStartSession: (arg: string | null) => void
   onCreateSession: () => void
   onDeleteSession: (arg: string | null) => void
@@ -12,10 +12,8 @@ type Props = {
 
 const CollabPickSession = ({ sessions, onStartSession, onCreateSession, onDeleteSession }: Props) => (
   <div className="text-center">
-    <UserGroupIcon className="h-40 w-40 mx-auto text-primary" />
-    <h3 className="text-xl font-bold leading-6 text-primary">
-      Colaboração ao vivo...
-    </h3>
+    <Users size="200" className=" mx-auto text-primary" />
+    <h3 className="text-xl font-bold leading-6 text-primary">Colaboração ao vivo...</h3>
 
     <p className="mt-6 text-sm text-gray-600">
       <span className="font-bold block text-gray-800">
@@ -25,12 +23,8 @@ const CollabPickSession = ({ sessions, onStartSession, onCreateSession, onDelete
     </p>
 
     <div className="flex justify-center mt-6 space-x-4 ">
-      <Button
-        variant="icon"
-        className="flex items-center rounded-lg bg-primary py-6"
-        onClick={onCreateSession}
-      >
-        <PlayCircleIcon className="h-8 w-8 mr-2 " />
+      <Button className="bg-primary" onClick={onCreateSession}>
+        <PlayCircle size="18" />
         Iniciar nova sessão
       </Button>
     </div>
@@ -38,26 +32,27 @@ const CollabPickSession = ({ sessions, onStartSession, onCreateSession, onDelete
     <div className="mt-6 text-center">
       <h4 className="text-md font-bold ">Sessões anteriores</h4>
       <p className="text-sm text-gray-600">
-        As sessões têm um tempo de vida, pelo que se não quiseres perder as tuas opções, terás de guardar para o teu dispositivo localmente.
+        As sessões têm um tempo de vida, pelo que se não quiseres perder as tuas opções, terás de guardar para o teu
+        dispositivo localmente.
       </p>
       <ul className="mt-4 flex flex-col sm:grid sm:grid-cols-1 sm:gap-y-4">
         {sessions.map((session) => (
-          <li key={session.id} className="sm:grid sm:grid-cols-7 flex flex-col sm:mt-0  mt-6 items-center text-sm text-gray-800 gap-4">
+          <li
+            key={session.id}
+            className="sm:grid sm:grid-cols-7 flex flex-col sm:mt-0  mt-6 items-center text-sm text-gray-800 gap-4"
+          >
             <span className="col-span-2 truncate whitespace-nowrap font-bold">{session.name}</span>
-            <span className="col-span-2 text-gray-600 truncate whitespace-nowrap">editado {toHumanReadableTimeDiff(session.lastEdited)}</span>
-            <span className="col-span-2 text-gray-600 truncate whitespace-nowrap">expira {toHumanReadableTimeDiff(session.expirationTime)}</span>
+            <span className="col-span-2 text-gray-600 truncate whitespace-nowrap">
+              editado {toHumanReadableTimeDiff(session.lastEdited)}
+            </span>
+            <span className="col-span-2 text-gray-600 truncate whitespace-nowrap">
+              expira {toHumanReadableTimeDiff(session.expirationTime)}
+            </span>
             <div className="col-span-1 flex justify-end space-x-4">
-              <a
-                href="#"
-                className="text-primary hover:underline"
-                onClick={() => onStartSession(session.id)}
-              >
+              <a href="#" className="text-primary hover:underline" onClick={() => onStartSession(session.id)}>
                 Entrar
               </a>
-              <button
-                className="text-primary hover:text-red-800"
-                onClick={() => onDeleteSession(session.id)}
-              >
+              <button className="text-primary hover:text-red-800" onClick={() => onDeleteSession(session.id)}>
                 &times;
               </button>
             </div>
@@ -66,6 +61,6 @@ const CollabPickSession = ({ sessions, onStartSession, onCreateSession, onDelete
       </ul>
     </div>
   </div>
-);
+)
 
-export default CollabPickSession;
+export default CollabPickSession
