8d5613965e63f59bb81ab1e45d2629abf0ecd97a
Pedro Lunet <pedrolunet@gmail.com>
2026-03-08 18:33:03 +0000

    updated tabs usage on main page

--- src/pages/TimeTableSelector.tsx
+++ src/pages/TimeTableSelector.tsx
@@ -6,19 +6,14 @@ import MajorContext from '../contexts/MajorContext'
 import { useSidebarContext } from '../components/layout/SidebarPosition'
 import { SidebarProvider } from '../components/layout/SidebarPosition'
 import PlannerSchedule from '../components/planner/schedule/PlannerSchedule'
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from "../components/ui/tabs"
-import Alert, { AlertType }  from '../components/planner/Alert'
+import { Tabs, TabsItem, TabsItems, TabsPanel, TabsPanels } from '../components/ui/new/tabs'
+import Alert, { AlertType } from '../components/planner/Alert'
 import { AlertDescription } from '../components/ui/alert'
 import SessionContext from '../contexts/SessionContext'
 import StorageAPI from '../api/storage'
-import { X } from "lucide-react" 
+import { X } from 'lucide-react'
 const TimeTableSelectorPage = () => {
-  const { setMajors } = useContext(MajorContext);
+  const { setMajors } = useContext(MajorContext)
 
   // fetch majors when component is ready
   useEffect(() => {
@@ -32,12 +27,12 @@ const TimeTableSelectorPage = () => {
     <SidebarProvider>
       <Content />
     </SidebarProvider>
-  );
-};
+  )
+}
 
 const Content = () => {
-  const { sidebarPosition } = useSidebarContext();
-  const { user } = useContext(SessionContext);
+  const { sidebarPosition } = useSidebarContext()
+  const { user } = useContext(SessionContext)
   const [showExchangeAlert, setShowExchangeAlert] = useState<boolean>(true)
 
   useEffect(() => {
@@ -50,70 +45,67 @@ const Content = () => {
   }
 
   return (
-    <div className='h-full w-full'>
+    <div className="h-full w-full">
       <div className="flex w-full  px-4 py-4 lg:hidden justify-items-start">
-       <Tabs defaultValue="planner" className="w-full">
-        <TabsList className="grid w-full grid-cols-2 ">
-          <TabsTrigger value="planner">Horário</TabsTrigger>
-          <TabsTrigger value="sidebar">Turmas</TabsTrigger>
-        </TabsList>
-        <TabsContent value="planner">
-          <div className="rounded-sm bg-lightest px-3 py-3 dark:bg-dark ">
-            <div className="h-full w-full ">
+        <Tabs className="w-full">
+          <TabsItems className="w-full">
+            <TabsItem className="flex-1">Horário</TabsItem>
+            <TabsItem className="flex-1">Turmas</TabsItem>
+          </TabsItems>
+          <TabsPanels>
+            <TabsPanel>
+              <div className="rounded-sm bg-lightest px-3 py-3 dark:bg-dark ">
+                <div className="h-full w-full ">
+                  <PlannerSchedule />
+                </div>
+              </div>
+            </TabsPanel>
+            <TabsPanel>
+              <Sidebar />
+            </TabsPanel>
+          </TabsPanels>
+        </Tabs>
+
+      </div>
+      <div className="hidden lg:grid w-full grid-cols-12 gap-x-4 gap-y-4 px-4 py-4">
+        {user?.eligible_exchange && showExchangeAlert && (
+          <div className="col-span-12 mb-2">
+            <Alert type={AlertType.info} className="relative p-4">
+              <AlertDescription className="pr-8">
+                Esta secção funciona apenas como Planner de horários. As inscrições e trocas reais são realizadas na
+                página de Inscrições e Trocas de Turmas.
+              </AlertDescription>
+              <button
+                onClick={handleCloseAlert}
+                className="absolute top-1.5 right-3 text-current opacity-70 hover:opacity-100 hover:bg-black/5 dark:hover:bg-white/10 p-1 rounded-full transition-all"
+              >
+                <X size={18} />
+              </button>
+            </Alert>
+          </div>
+        )}
+        {sidebarPosition === 'left' ? (
+          <>
+            <div className="col-span-12 lg:col-span-3 min-h">
+              <Sidebar />
+            </div>
+            <div className="col-span-12 lg:col-span-9 min-h rounded-md bg-lightest px-3 py-3 dark:bg-dark 2xl:px-5 2xl:py-5">
               <PlannerSchedule />
             </div>
-          </div>
-        </TabsContent>
-        <TabsContent value="sidebar"> 
-          <Sidebar />
-            
-            </TabsContent>
-        </Tabs>
-      {/* Tabs */}
-      {/* Schedule Preview */}
-      
+          </>
+        ) : (
+          <>
+            <div className="col-span-12 lg:col-span-9 min-h rounded-md bg-lightest px-3 py-3 dark:bg-dark 2xl:px-5 2xl:py-5">
+              <PlannerSchedule />
+            </div>
+            <div className="col-span-12 lg:col-span-3 min-h">
+              <Sidebar />
+            </div>
+          </>
+        )}
+      </div>
     </div>
-    <div className="hidden lg:grid w-full grid-cols-12 gap-x-4 gap-y-4 px-4 py-4">
-      {user?.eligible_exchange && showExchangeAlert && (
-        <div className="col-span-12 mb-2">
-          <Alert type={AlertType.info} className="relative p-4">
-            <AlertDescription className="pr-8"> 
-              Esta secção funciona apenas como Planner de horários. As inscrições e trocas reais são realizadas na página de Inscrições e Trocas de Turmas.
-            </AlertDescription>
-            <button
-              onClick={handleCloseAlert}
-              className="absolute top-1.5 right-3 text-current opacity-70 hover:opacity-100 hover:bg-black/5 dark:hover:bg-white/10 p-1 rounded-full transition-all"
-            >
-              <X size={18} />
-            </button>
-          </Alert>
-        </div>
-      )}
-      {sidebarPosition === 'left' ? (
-        <>
-          <div className='col-span-12 lg:col-span-3 min-h'>
-            <Sidebar />
-          </div>
-          <div className='col-span-12 lg:col-span-9 min-h rounded-md bg-lightest px-3 py-3 dark:bg-dark 2xl:px-5 2xl:py-5'>
-            <PlannerSchedule />
-          </div>
-        </>
-      ) : (
-        <>
-          <div className='col-span-12 lg:col-span-9 min-h rounded-md bg-lightest px-3 py-3 dark:bg-dark 2xl:px-5 2xl:py-5'>
-            <PlannerSchedule />
-          </div>
-          <div className='col-span-12 lg:col-span-3 min-h'>
-            <Sidebar />
-          </div>
-        </>
-      )}
-    </div>
-
-    </div>
-    
   )
 }
 
-
-export default TimeTableSelectorPage;
+export default TimeTableSelectorPage
