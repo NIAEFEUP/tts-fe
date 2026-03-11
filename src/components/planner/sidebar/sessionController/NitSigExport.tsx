b9b4f88a361cb7b5d5b36531be7c2026b4d7e25f
Pedro Lunet <pedrolunet@gmail.com>
2026-03-11 16:43:18 +0000

    updated tooltip usage on various compoentns

--- src/components/planner/sidebar/sessionController/NitSigExport.tsx
+++ src/components/planner/sidebar/sessionController/NitSigExport.tsx
@@ -1,26 +1,24 @@
-import { NitSigIconSVG } from "../../../svgs"
-import { TooltipProvider, Tooltip, TooltipTrigger, TooltipContent  } from "../../../ui/tooltip"
+import { NitSigIconSVG } from '../../../svgs'
+import { Tooltip, TooltipTrigger, TooltipContent } from '../../../ui/new/tooltip'
 
 /**
  * Future feature that allows the user to export his schedule options to NitSig
  */
 const NitSigExport = () => {
   return (
-    <TooltipProvider delayDuration={300}>
-      <Tooltip>
-        <TooltipTrigger asChild>
-          <button
-            onClick={() => {}}
-            className="group flex w-full items-center gap-2 rounded-md p-1 text-sm text-white-900 disabled:cursor-not-allowed disabled:opacity-50"
-            disabled
-          >
-            <NitSigIconSVG className="h-4 w-4 black:brightness-150 rounded-sm" />
-            <span className=" dark:text-white">Exportar para o Sigarra</span>
-          </button>
-        </TooltipTrigger>
-        <TooltipContent side="bottom">Disponível em breve</TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <Tooltip delayIn={300}>
+      <TooltipTrigger asChild>
+        <button
+          onClick={() => {}}
+          className="group flex w-full items-center gap-2 rounded-md p-1 text-sm text-white-900 disabled:cursor-not-allowed disabled:opacity-50"
+          disabled
+        >
+          <NitSigIconSVG className="h-4 w-4 black:brightness-150 rounded-sm" />
+          <span className=" dark:text-white">Exportar para o Sigarra</span>
+        </button>
+      </TooltipTrigger>
+      <TooltipContent className="z-100">Disponível em breve</TooltipContent>
+    </Tooltip>
   )
 }
 
--- src/components/planner/sidebar/sessionController/course-picker/Ects.tsx
+++ src/components/planner/sidebar/sessionController/course-picker/Ects.tsx
@@ -1,14 +1,12 @@
 import { useContext } from 'react'
 import { InformationCircleIcon } from '@heroicons/react/24/solid'
 import CoursePickerContext from '../../../../../contexts/coursePicker/CoursePickerContext'
-import { TooltipTrigger, TooltipContent, TooltipProvider, Tooltip } from '../../../../ui/tooltip'
+import { TooltipTrigger, TooltipContent, Tooltip } from '../../../../ui/new/tooltip'
 
 const Ects = () => {
   const { coursesStorage } = useContext(CoursePickerContext)
 
-  const totalEcts = coursesStorage.reduce(
-    (acc, course) => acc + parseFloat(String(course.ects).replace(',', '.')
-    ), 0)
+  const totalEcts = coursesStorage.reduce((acc, course) => acc + parseFloat(String(course.ects).replace(',', '.')), 0)
   const warning = totalEcts > 42 ? 2 : totalEcts > 36 ? 1 : 0
 
   return (
@@ -16,20 +14,18 @@ const Ects = () => {
       <div className="text-sm">Total ECTS:</div>
       <div className="font-semibold text-sm">{totalEcts}</div>
       {warning ? (
-        <TooltipProvider delayDuration={300}>
-          <Tooltip>
-            <TooltipTrigger asChild>
-              <InformationCircleIcon
-                className={`ml-1 h-6 w-6 transition ${warning === 1 ? 'text-amber-500' : 'text-red-600'}`}
-              />
-            </TooltipTrigger>
-            <TooltipContent>
-              {warning === 1
-                ? "Nós recomendamos escolher 36 ou menos ECTS."
-                : "Por norma, o limite de créditos por semestre é 42 ECTS."}
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
+        <Tooltip delayIn={300}>
+          <TooltipTrigger asChild>
+            <InformationCircleIcon
+              className={`ml-1 h-6 w-6 transition ${warning === 1 ? 'text-amber-500' : 'text-red-600'}`}
+            />
+          </TooltipTrigger>
+          <TooltipContent>
+            {warning === 1
+              ? 'Nós recomendamos escolher 36 ou menos ECTS.'
+              : 'Por norma, o limite de créditos por semestre é 42 ECTS.'}
+          </TooltipContent>
+        </Tooltip>
       ) : (
         <></>
       )}
