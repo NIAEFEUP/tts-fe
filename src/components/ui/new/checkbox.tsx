5f9ebffb1be4e344cfb5581b6bfe689d81ed9aa1
Pedro Lunet <pedrolunet@gmail.com>
2026-05-30 09:52:10 +0100

    upgraded checkbox

--- src/components/ui/new/checkbox.tsx
+++ src/components/ui/new/checkbox.tsx
@@ -1,47 +1,29 @@
-'use client'
+import { useEffect, useRef } from "react"
 
-import { useEffect, useRef } from 'react'
-import { VariantProps } from 'cva'
+import { composeRefs } from "@/lib/compose-refs"
+import { cva } from "@/lib/utils"
 
-import { composeRefs } from '../../../lib/compose-refs'
-import { cva } from '../../../lib/utils'
-
-const checkboxStyle = cva({
-  base: [
-    'appearance-none relative size-5 shrink-0 outline-none rounded-sm border border-border bg-background shadow-xs enabled:cursor-pointer enabled:not-checked:hover:border-mix-border/8 flex items-center justify-center focus-visible:ring-4 ring-ring transition',
-    // checked checkmark
-    "before:absolute checked:before:content-['✓'] before:text-white checked:before:text-xs checked:before:font-bold",
-    // indeterminate dash
-    'indeterminate:before:w-1.5 indeterminate:before:h-0.5 indeterminate:before:bg-white',
-    // disabled
-    'disabled:cursor-not-allowed disabled:border-foreground/5 disabled:bg-foreground/10 disabled:checked:before:text-foreground/50 disabled:indeterminate:before:bg-foreground/50',
-  ],
-  variants: {
-    variant: {
-      accent: [
-        'checked:enabled:border-accent checked:enabled:bg-accent checked:enabled:hover:border-mix-accent/8 checked:enabled:hover:mix-with-accent-foreground',
-        'indeterminate:enabled:border-accent indeterminate:enabled:bg-accent indeterminate:enabled:hover:border-mix-accent/8 indeterminate:enabled:hover:mix-with-accent-foreground',
-        'before:text-accent-foreground',
-        'indeterminate:before:bg-accent-foreground',
-      ],
-      primary: [
-        'checked:enabled:border-primary checked:enabled:bg-primary checked:enabled:hover:border-primary/90',
-        'indeterminate:enabled:border-primary indeterminate:enabled:bg-primary indeterminate:enabled:hover:border-primary/90',
-        'before:text-white',
-        'indeterminate:before:bg-white',
-      ],
-    },
-  },
-  defaultVariants: {
-    variant: 'accent',
-  },
-})
-
-interface CheckboxProps extends Omit<React.ComponentPropsWithRef<'input'>, 'type'>, VariantProps<typeof checkboxStyle> {
+interface CheckboxProps extends Omit<React.ComponentPropsWithRef<"input">, "type"> {
   indeterminate?: boolean
 }
 
-const Checkbox = ({ ref, indeterminate, variant, className, ...props }: CheckboxProps) => {
+const checkboxStyle = cva({
+  base: [
+    "focus-visible:ring-(length:--ring-width) relative flex size-5 shrink-0 appearance-none items-center justify-center rounded-sm border border-border bg-background shadow-xs outline-none ring-ring transition enabled:cursor-pointer enabled:not-checked:hover:border-mix-border/8",
+    // checked
+    "checked:enabled:hover:mix-with-accent-foreground checked:enabled:border-accent checked:enabled:bg-accent checked:enabled:hover:border-mix-accent/8",
+    // indeterminate
+    "indeterminate:enabled:hover:mix-with-accent-foreground indeterminate:enabled:border-accent indeterminate:enabled:bg-accent indeterminate:enabled:hover:border-mix-accent/8",
+    // checked checkmark
+    "before:absolute before:text-accent-foreground checked:before:font-bold checked:before:text-xs checked:before:content-['✓']",
+    // indeterminate dash
+    "indeterminate:before:h-0.5 indeterminate:before:w-1.5 indeterminate:before:bg-accent-foreground",
+    // disabled
+    "disabled:cursor-not-allowed disabled:border-foreground/5 disabled:bg-foreground/10 disabled:indeterminate:before:bg-foreground/50 disabled:checked:before:text-foreground/50",
+  ],
+})
+
+const Checkbox = ({ ref, indeterminate, className, ...props }: CheckboxProps) => {
   const internalRef = useRef<HTMLInputElement>(null)
 
   useEffect(() => {
@@ -51,12 +33,7 @@ const Checkbox = ({ ref, indeterminate, variant, className, ...props }: Checkbox
   }, [indeterminate])
 
   return (
-    <input
-      type="checkbox"
-      ref={composeRefs(ref, internalRef)}
-      className={checkboxStyle({ variant, className })}
-      {...props}
-    />
+    <input type="checkbox" ref={composeRefs(ref, internalRef)} className={checkboxStyle({ className })} {...props} />
   )
 }
 
