4efe82f95ae7d988dbf81d1e2b2dcf222a2de06f
Pedro Lunet <pedrolunet@gmail.com>
2026-08-26 12:03:16 +0100

    applied number badges

--- src/components/planner/sidebar/OptionsController.tsx
+++ src/components/planner/sidebar/OptionsController.tsx
@@ -17,7 +17,7 @@ const OptionsController = () => {
 
   return (
     <ReactSortable
-      className="m-y-2 flex flex-row justify-center gap-2 overflow-x-auto py-2 text-center w-full lg:justify-start"
+      className="m-y-2 flex flex-row justify-center gap-2 py-2 px-1 text-center w-full lg:justify-start !overflow-visible"
       list={multipleOptions}
       setList={(newMultipleOptions) => {
         const prevId = multipleOptions[selectedOption].id
@@ -50,24 +50,28 @@ type Props = {
 
 const OptionButton = ({ option }: Props) => {
   const { multipleOptions, selectedOption, setSelectedOption } = useContext(MultipleOptionsContext)
+  const index = multipleOptions.findIndex((currentOption) => currentOption.id === option.id)
 
   return (
     <Tooltip>
       <Tooltip.Trigger
         asChild
         onClick={() => {
-          setSelectedOption(multipleOptions.findIndex((currentOption) => currentOption.id === option.id))
+          setSelectedOption(index)
         }}
       >
         <Button
           square
           size="md"
-          className={`p-0 pr-1.5 pt-1.5 hover:bg-primary/75 ${multipleOptions[selectedOption].id === option.id ? 'bg-primary/75 dark:bg-primary/50' : 'bg-lightish dark:bg-darkish'}`}
+          className={`relative p-1 hover:bg-primary/75 ${multipleOptions[selectedOption].id === option.id ? 'bg-primary/75 dark:bg-primary/50' : 'bg-lightish dark:bg-darkish'}`}
         >
           <Blobatar
             name={option.name}
             animate="always"
           />
+          <span className="absolute -top-1 -right-1 flex items-center justify-center min-w-4 h-4 rounded-lg  bg-secondary text-[10px] font-bold text-white dark:text-gray-900 leading-none z-10 shadow-md">
+            {index + 1}
+          </span>
         </Button>
       </Tooltip.Trigger>
       <Tooltip.Content className="truncate">{option.name}</Tooltip.Content>
