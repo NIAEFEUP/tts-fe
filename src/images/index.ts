1d4d7bbae78204df5f1691cc93ec92b75726ce64
Pedro Castro <falecompedroac@gmail.com>
2026-09-07 12:14:48 +0100

    cleanup images folder

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,31 +1,13 @@
-import AppLogo from './icon.png'
-import HomePageImage from './homepage1.png'
-import HomePageAltImage from './homepage2.jpg'
-import LogoFEDarkImage from './logo-dark.png'
-import LogoFELightImage from './logo-light.png'
 import LogoNIAEFEUPImage from './logo-ni.png'
-import LogoNIAEFEUPAltImage from './logo-ni2.png'
 import StampNIAEFEUPImage from './ni-stamp.png'
 import BackStampNIAEFEUPImage from './ni-stamp-back.png'
 import ScheduleLightImage from './schedule-light.png'
 import ScheduleDarkImage from './schedule-dark.png'
-import NoticeOldScheduleImage from './old-new-schedule.jpg'
-import SelectNewScheduleImage from './select-new-schedule.jpg'
-import WaitNewScheduleImage from './wait-new-schedule.jpg'
 
 export {
-  AppLogo,
-  HomePageImage,
-  HomePageAltImage,
-  LogoFEDarkImage,
-  LogoFELightImage,
   LogoNIAEFEUPImage,
-  LogoNIAEFEUPAltImage,
   StampNIAEFEUPImage,
   BackStampNIAEFEUPImage,
   ScheduleLightImage,
   ScheduleDarkImage,
-  NoticeOldScheduleImage,
-  SelectNewScheduleImage,
-  WaitNewScheduleImage,
 }
