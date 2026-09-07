d526f756fbc791f01b584e0660a8a159859815ce
Pedro Castro <falecompedroac@gmail.com>
2026-09-07 12:13:46 +0100

    cleanup components modules

--- src/components/planner/index.ts
+++ src/components/planner/index.ts
@@ -1,6 +1,5 @@
 import Alert from './Alert'
 import Schedule from './Schedule'
 import Sidebar from './Sidebar'
-import ResetDataModal from './ResetDataModal'
 
-export { Schedule, Alert, Sidebar, ResetDataModal }
+export { Schedule, Alert, Sidebar }
